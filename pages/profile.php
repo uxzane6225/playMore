bfdc61e5daf377b366a9e96ec5f96badec6f04b0
uxzane6225 <132722108+uxzane6225@users.noreply.github.com>
2026-06-24 10:40:30 +0800

    Added Profile Edit Design

--- pages/profile.php
+++ pages/profile.php
@@ -13,10 +13,50 @@ include('templates/navbar.php');
         <p><?= isset($_SESSION['bio']) ? $_SESSION['bio'] : "No description"; ?></p>
     </div>
 </header>
-<main class="m-5 p-5">
-    
+<main class="m-5 p-5 flex flex-col gap-5">
+    <h2 class="text-2xl font-bold">Profile Settings</h2>
+    <form action="../processors/profile/edit.php" class="flex flex-col gap-2 text-lg">
+        <div class="flex flex-col gap-1">
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" class="p-2.5 text-black rounded-lg border outline-none lg:border-gray-400 lg:focus:outline-red-400"  value="<?= isset($_SESSION['oldUser']) ? $_SESSION['oldUser'] : $_SESSION['username'] ?>">
+            <?php if(isset($_SESSION['userError'])): ?>
+                <p class="text-end text-xs"><?= $_SESSION['userError']; ?></p>
+                <?php unset($_SESSION['userError']); ?>
+            <?php endif; ?>
+        </div>
+
+        <div class="flex flex-col gap-1">
+            <label for="bio">Bio</label>
+            <input type="text" id="bio" name="bio" class="p-2.5 text-black rounded-lg border outline-none lg:border-gray-400 lg:focus:outline-red-400"  value="<?= isset($_SESSION['oldBio']) ? $_SESSION['oldBio'] : $_SESSION['bio'] ?>">
+            <?php if(isset($_SESSION['bioError'])): ?>
+                <p class="text-end text-xs"><?= $_SESSION['bioError']; ?></p>
+                <?php unset($_SESSION['bioError']); ?>
+            <?php endif; ?>
+        </div>
+
+        <div class="flex flex-col gap-1">
+            <label for="gender">Gender</label>
+            <select id="gender" name="gender" class="p-2.5 text-black rounded-lg border outline-none lg:border-gray-400 lg:focus:outline-red-400">
+                <option value="male" class="w-fit">Male</option>
+                <option value="female" class="w-fit">Female</option>
+            </select>
+            <?php if(isset($_SESSION['genderError'])): ?>
+                <p class="text-end text-xs"><?= $_SESSION['genderError']; ?></p>
+                <?php unset($_SESSION['genderError']); ?>
+            <?php endif; ?>
+        </div>
+
+        <div class="flex flex-col">
+            <label for="birth" class="pl-2">Birthday</label>
+            <input type="date" id="birth" name="birth" value="<?= isset($_SESSION['oldBirthdate']) ? $_SESSION['oldBirthdate'] : $_SESSION['birthdate'] ?>" class="p-2.5 text-black rounded-lg border outline-none lg:border-gray-400 lg:focus:outline-red-400">
+            <?php if(isset($_SESSION['birthError'])): ?>
+                <p class="text-end text-xs"><?= $_SESSION['birthError']; ?></p>
+                <?php unset($_SESSION['birthError']); ?>
+            <?php endif; ?>
+        </div>
+    </form>
 </main>
-<footer class="p-10 text-white text-center bg-red-700">
+<footer class="p-10 text-white text-center bg-red-600">
     <p>&copy; playMore.test. Rights Reserved <?= date('Y') ?></p>
 </footer>
 <?php include('templates/foot.php'); ?>
